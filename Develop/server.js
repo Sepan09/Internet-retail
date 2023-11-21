50fbdb1efb527431a58246749e618c1cae161d9f
sepan mohammed <sepanmustafa@gmail.com>
2023-11-21 00:08:43 -0600

    worked on routes and category-routes.js, created findall, findbypk, create, destroy

--- Develop/server.js
+++ Develop/server.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const routes = require('./routes');
-// import sequelize connection
 const sequelize = require('sequelize');
+// import sequelize connection
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -12,6 +13,12 @@ app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
+sequelize.sync().then(() => {
+  app.listen(PORT, () =>console.log(`App listening on port ${PORT}!`));
 });
+
+
+
+// app.listen(PORT, () => {
+//   console.log(`App listening on port ${PORT}!`);
+// });
